979a768f03be43e902f4832c2b517b20828b4fd2
Oleksander Kostenko <o.kostenko@samsung.com>
2016-05-20 11:38:22 +0300

    TizenRefApp-5512 App Notifications Radiobuttons State isn't Saved
    Change-Id: I088bcf2f9a70c0507195892b3da8daf6c6631496
    Signed-off-by: Oleksander Kostenko <o.kostenko@samsung.com>

--- src/common-efl.c
+++ src/common-efl.c
@@ -221,25 +221,28 @@ static Evas_Object* gl_content_get_cb(void *data, Evas_Object *obj, const char *
 		return icon;
 	}
 
-	if (!strcmp(part, "elm.swallow.end")) {
-		content = elm_check_add(obj);
-		elm_object_style_set(content, "on&off");
-		elm_check_state_set(content, data_list->do_not_disturb_except);
-		evas_object_show(content);
-		evas_object_pass_events_set(content, 1);
-		evas_object_smart_callback_add(content, "changed", _excepted_check_changed_cb, data_list);
-		evas_object_propagate_events_set(content, 0);
-		return content;
-	} else if (data_list->item_style == ITEM_STYLE_ONE_LINE && !strcmp(part, "elm.icon.right")) {
-		content = elm_check_add(obj);
-		elm_object_style_set(content, "on&off");
-		elm_check_state_set(content, data_list->allow_to_notify);
-		evas_object_show(content);
-		evas_object_pass_events_set(content, 1);
-		evas_object_smart_callback_add(content, "changed", _allow_to_nofity_check_changed_cb, data_list);
-		evas_object_propagate_events_set(content, 0);
-		return content;
-	}
+    if(!strcmp(part, "elm.swallow.end")) {
+        if(data_list->item_style == ITEM_STYLE_DEFAULT) {
+            content = elm_check_add(obj);
+            elm_object_style_set(content, "on&off");
+            elm_check_state_set(content, data_list->do_not_disturb_except);
+            evas_object_show(content);
+            evas_object_pass_events_set(content, 1);
+            evas_object_smart_callback_add(content, "changed", _excepted_check_changed_cb, data_list);
+            evas_object_propagate_events_set(content, 0);
+            return content;
+        }
+        else if(data_list->item_style == ITEM_STYLE_ONE_LINE) {
+            content = elm_check_add(obj);
+            elm_object_style_set(content, "on&off");
+            elm_check_state_set(content, data_list->allow_to_notify);
+            evas_object_show(content);
+            evas_object_pass_events_set(content, 1);
+            evas_object_smart_callback_add(content, "changed", _allow_to_nofity_check_changed_cb, data_list);
+            evas_object_propagate_events_set(content, 0);
+            return content;
+        }
+    }
 
 	return content;
 }
