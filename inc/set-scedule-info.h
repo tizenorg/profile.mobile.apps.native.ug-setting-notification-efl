90087728548a565ead752e15b72b6e0a5fc1e85c
Oleksander Kostenko <o.kostenko@samsung.com>
2016-07-15 15:27:53 +0300

    TizenRefApp-6521 Do not Disturb Schedule isn't Saved
    Change-Id: I119cb683c02ae0af21916ba1ff1b5b3531843fdd
    Signed-off-by: Oleksander Kostenko <o.kostenko@samsung.com>

--- inc/set-scedule-info.h
+++ inc/set-scedule-info.h
@@ -25,9 +25,47 @@
 #include "log.h"
 #include "common-efl.h"
 
+/**
+ * @brief Call back when schedule state is changed
+ */
 void set_schedule_check_changed_cb(void *data, Evas_Object *obj, void *event_info);
+/**
+ * @brief Create full set schedule view
+ * @param[in] data User created ug data
+ */
 void gl_set_schedule_selected(ug_data *data);
+/**
+ * @brief Get state of schedule
+ * @retval Returns true if schedule is enabled, else false
+ */
 bool get_schedule();
+/**
+ * @brief Create start or end time item with time picker
+ * @param[in] parent Genlist
+ * @param[in] is_start_time_item  Set true if is start time item, false if end time
+ * @retval Returns layout with time item
+ */
 Evas_Object* start_end_time_item(Evas_Object* parent, bool is_start_time_item);
+
+/**
+ * @brief Check if end time is earlier then start time
+ * @retval Returns true if end time is earlier, else false
+ */
+bool is_next_day();
+/**
+ * @brief Forms a string with the end and start time of schedule
+ * @retval Returns string
+ */
+const char *get_time_string();
+/**
+ * @brief Forms a string with the selected days of the week
+ * @retval Returns string
+ */
+const char *get_day_string();
+/**
+ * @brief Get time format from system settings
+ * @retval Returns 12H or 24H time format
+ */
+int get_time_format();
 
 #endif //__SET_SCHEDULE_INFO_H__
